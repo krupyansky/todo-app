556ac536ce0fd8d75af3969f1095395039dda7a6
krupy <krupy@positron-it.ru>
2021-07-25 15:45:53 +0300

    Make sign in + jwt tokens

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -25,3 +25,12 @@ func (r *AuthPostgres) CreateUser(user entity.User) (int, error) {
 
 	return id, nil
 }
+
+func (r *AuthPostgres) GetUser(username, password string) (entity.User, error) {
+	var user entity.User
+
+	query := fmt.Sprintf("SELECT id FROM %s WHERE username=$1 AND password_hash=$2", usersTable)
+	err := r.db.Get(&user, query, username, password)
+
+	return user, err
+}
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,11 +3,22 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/krupyansky/todo-app/pkg/entity"
 	"github.com/krupyansky/todo-app/pkg/repository"
+	"time"
 )
 
-const salt = "rl43hEl542@#5sD%$"
+const (
+	salt       = "rl43hEl542@#5sD%$"
+	signingKey = "6MkQ0$nbP12flJqSlxNAYrg4"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,6 +31,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user entity.User) (int, error) {
 	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
